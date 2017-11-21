6c062b6d5e813b4442f158c528f190c9684ab59b
kutyel <flaviocorpa@gmail.com>
2017-11-21 15:13:20 +0100

    Remove pug 🚫🐶

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,18 +1,19 @@
-<template lang="pug">
-  .note
-    input(type="checkbox" :id="note.id" :checked="note.completed")
-    p {{ note.title }}
+<template>
+  <div class="note">
+    <input type="checkbox" :id="note.id" :checked="note.completed" />
+    <p>{{ note.title }}</p>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Note',
-  props: ['note']
-}
+  export default {
+    name: 'Note',
+    props: ['note']
+  }
 </script>
 
 <style>
-p {
-  display: inline-block;
-}
+  p {
+    display: inline-block;
+  }
 </style>
--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,35 +1,37 @@
-<template lang="pug">
-    ul
-      li(v-for="note in notes" :key="note.id")
-        Note(:note="note")
+<template>
+    <ul>
+      <li v-for="note in notes" :key="note.id">
+        <Note :note="note" />
+      </li>
+    </ul>
 </template>
 
 <script>
-import Note from './Note'
+  import Note from './Note'
 
-export default {
-  name: 'NoteList',
-  components: { Note },
-  data: () => ({ notes: [] }),
-  async created () {
-    const res = await fetch('http://weslezw230.230.axc.nl/notes')
-    const { data } = await res.json()
-    this.notes = data
+  export default {
+    name: 'NoteList',
+    components: { Note },
+    data: () => ({ notes: [] }),
+    async created () {
+      const res = await fetch('http://weslezw230.230.axc.nl/notes')
+      const { data } = await res.json()
+      this.notes = data
+    }
   }
-}
 </script>
 
 <style scoped>
-ul {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 </style>
